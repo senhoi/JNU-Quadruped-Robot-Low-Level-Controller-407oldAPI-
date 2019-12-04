11c68c1b81a22d4741a3eac8811726e37198b4c7
senhoi <zhangchunyu934@gmail.com>
2019-12-04 00:48:20 -0800

    .

--- USER/tasks.c
+++ USER/tasks.c
@@ -37,7 +37,7 @@ void init_task_controller(void)
     MOTOR_init(&SCA[0], MOTOR_VELOCITY_MODE);
 
     MOTOR_set_vel_loop_gain(&SCA[0], 0.1f, 0.0f);
-    MOTOR_set_vel_loop_limit(&SCA[0], 33.0f, -33.0f);
+    MOTOR_set_vel_loop_limit(&SCA[0], 3.30f, -3.30f);
     MOTOR_set_pos_loop_gain(&SCA[0], 1.0f, 0.0f, 0.0f);
     MOTOR_set_pos_loop_limit(&SCA[0], 0.0f, 0.0f);
 }
@@ -105,27 +105,27 @@ void ctrl_task(void)
         MOTOR_calc(&SCA[i]);
 
         SetActrCurrent(MOTOR_get_cmd(&SCA[i]) / 33.0f, devIDList[i]);
-        printf("DEBUG3:CUR:%.3fA\r\n", MOTOR_get_cmd(&SCA[i]));
-        delay_ms(1);
+        //printf("DEBUG3:CUR:%.3fA\r\n", MOTOR_get_cmd(&SCA[i]));
+        //delay_ms(1);
 
         printf("CUR-PID:\r\n");
-        printf("K:%.2f %.2f %.2f\r\n", SCA[0].pid_cur.kp, SCA[0].pid_cur.ki, SCA[0].pid_cur.kd);
+        //printf("K:%.2f %.2f %.2f\r\n", SCA[0].pid_cur.kp, SCA[0].pid_cur.ki, SCA[0].pid_cur.kd);
         printf("in:%.2f fbk:%.2f ffd:%.2f\r\n", SCA[0].pid_cur.in, SCA[0].pid_cur.fbk, SCA[0].pid_cur.ffd);
         printf("out:%.2f %.2f\r\n", SCA[0].pid_cur.out[0], SCA[0].pid_cur.out[1]);
-        printf("err:%.2f %.2f %.2f\r\n", SCA[0].pid_cur.err[0], SCA[0].pid_cur.err[1], SCA[0].pid_cur.err[2]);
-        printf("max:%.2f min:%.2f err_sum:%.2f\r\n", SCA[0].pid_cur.max, SCA[0].pid_cur.min, SCA[0].pid_cur.err_sum);
+        //printf("err:%.2f %.2f %.2f\r\n", SCA[0].pid_cur.err[0], SCA[0].pid_cur.err[1], SCA[0].pid_cur.err[2]);
+        //printf("max:%.2f min:%.2f err_sum:%.2f\r\n", SCA[0].pid_cur.max, SCA[0].pid_cur.min, SCA[0].pid_cur.err_sum);
         printf("VEL-PID:\r\n");
         printf("K:%.2f %.2f %.2f\r\n", SCA[0].pid_vel.kp, SCA[0].pid_vel.ki, SCA[0].pid_vel.kd);
         printf("in:%.2f fbk:%.2f ffd:%.2f\r\n", SCA[0].pid_vel.in, SCA[0].pid_vel.fbk, SCA[0].pid_vel.ffd);
         printf("out:%.2f %.2f\r\n", SCA[0].pid_vel.out[0], SCA[0].pid_vel.out[1]);
         printf("err:%.2f %.2f %.2f\r\n", SCA[0].pid_vel.err[0], SCA[0].pid_vel.err[1], SCA[0].pid_vel.err[2]);
         printf("max:%.2f min:%.2f err_sum:%.2f\r\n", SCA[0].pid_vel.max, SCA[0].pid_vel.min, SCA[0].pid_vel.err_sum);
-        printf("POS-PID:\r\n");
-        printf("K:%.2f %.2f %.2f\r\n", SCA[0].pid_pos.kp, SCA[0].pid_pos.ki, SCA[0].pid_pos.kd);
-        printf("in:%.2f fbk:%.2f ffd:%.2f\r\n", SCA[0].pid_pos.in, SCA[0].pid_pos.fbk, SCA[0].pid_pos.ffd);
-        printf("out:%.2f %.2f\r\n", SCA[0].pid_pos.out[0], SCA[0].pid_pos.out[1]);
-        printf("err:%.2f %.2f %.2f\r\n", SCA[0].pid_pos.err[0], SCA[0].pid_pos.err[1], SCA[0].pid_pos.err[2]);
-        printf("max:%.2f min:%.2f err_sum:%.2f\r\n", SCA[0].pid_pos.max, SCA[0].pid_pos.min, SCA[0].pid_pos.err_sum);
+        //printf("POS-PID:\r\n");
+        //printf("K:%.2f %.2f %.2f\r\n", SCA[0].pid_pos.kp, SCA[0].pid_pos.ki, SCA[0].pid_pos.kd);
+        //printf("in:%.2f fbk:%.2f ffd:%.2f\r\n", SCA[0].pid_pos.in, SCA[0].pid_pos.fbk, SCA[0].pid_pos.ffd);
+        //printf("out:%.2f %.2f\r\n", SCA[0].pid_pos.out[0], SCA[0].pid_pos.out[1]);
+        //printf("err:%.2f %.2f %.2f\r\n", SCA[0].pid_pos.err[0], SCA[0].pid_pos.err[1], SCA[0].pid_pos.err[2]);
+        //printf("max:%.2f min:%.2f err_sum:%.2f\r\n", SCA[0].pid_pos.max, SCA[0].pid_pos.min, SCA[0].pid_pos.err_sum);
     }
 }
 
